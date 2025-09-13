c6508a4618c88826672838393db625ba2d79618e
clds13 <cindyy260027@gmail.com>
2025-09-13 09:32:20 -0400

    Extracted method multiples from Multiples class

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,6 +2,10 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        extracted();
+    }
+
+    private static void extracted() {
         int numberOfMultiples = 0;
         for (int i = 0; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
