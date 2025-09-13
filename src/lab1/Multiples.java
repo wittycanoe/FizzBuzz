7cc71fc60b3ce313ba9baba30f78c10dfc4690b5
clds13 <cindyy260027@gmail.com>
2025-09-13 09:42:51 -0400

    Overloaded multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,16 +2,26 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
-        extracted();
+        System.out.println(multiples());
     }
 
-    private static void extracted() {
+    static int multiples() {
         int numberOfMultiples = 0;
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 1; i < 1000; i++) {
             if (i % 3 == 0 || i % 5 == 0) {
                 numberOfMultiples++;
             }
         }
-        System.out.println(numberOfMultiples);
+        return numberOfMultiples;
+    }
+
+    static int multiples(int n, int a, int b) {
+        int numberOfMultiples = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                numberOfMultiples++;
+            }
+        }
+        return numberOfMultiples;
     }
 }
--- test/lab1/MultiplesDefaultTest.java
+++ test/lab1/MultiplesDefaultTest.java
@@ -8,8 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class MultiplesDefaultTest {
 
-    // TODO: When you are ready to test task X,
-    //  copy this file into the test folder.
     @Test
     public void testDefault() {
         assertEquals(466,
